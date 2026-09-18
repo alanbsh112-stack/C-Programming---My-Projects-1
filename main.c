05f82933bb278dfa1e3d152b64599261b5fe5e46
Alan Shaaban <alanbsh112@gmail.com>
2026-09-18 00:24:05 -0400

    "Wrote shoppping cart program"

--- main.c
+++ main.c
@@ -5,29 +5,29 @@
 int main()
 {
 
-    int age = 0;
-    float gpa = 0.0f;
-    char grade = '\0';
-    char name[50] = "";
+    // Shopping Cart Program
 
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
 
-    printf("Enter your gpa: ");
-    scanf("%f", &gpa);
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
 
-    printf("Enter your grade: ");
-    scanf(" %c", &grade);
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
 
-    getchar();
-    printf("Enter your full name: ");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) - 1] = '\0';
+    printf("How many would you like?: ");
+    scanf("%d", &quantity);
 
-    printf("%s\n", name);
-    printf("%d\n", age);
-    printf("%f\n", gpa);
-    printf("%c\n", grade);
+    total = price * quantity;
+
+    printf("\nYou have bought %d %s/s \n", quantity, item);
+
+    printf("The total is: %c%.2f", currency, total);
 
     return 0;
 }
