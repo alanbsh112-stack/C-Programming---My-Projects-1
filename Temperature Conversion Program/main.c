0dbd5c0979cd1b450f27736e03d5a8d09083ac8e
Alan Shaaban <alanbsh112@gmail.com>
2026-09-23 16:46:12 -0400

    Programmed Temperature Converter in c

---
+++ Temperature Conversion Program/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    // TEMPERATURE CONVERSION PROGRAM
+
+    char choice = '\0';
+    float fahrenheit = 0.0f;
+    float celsius = 0.0f;
+    printf("Temperature Conversion Program\n");
+    printf("C. Celsius to Fahrenheit\n");
+    printf("F. Fahrenheit to Celsius\n");
+    printf("Is the temperature in Celsius (C) or Fahrenheit (F)?: ");
+    scanf("%c", &choice);
+
+    if (choice == 'C')
+    {
+        // C to F
+        printf("Enter the temperature in Celsius: ");
+        scanf("%f", &celsius);
+        fahrenheit = (celsius * 9 / 5) + 32;
+        printf("%.1f Celsius is equal to %.1f Fahrenheit", celsius, fahrenheit);
+    }
+    else if (choice == 'F')
+    {
+        // F to C
+        printf("Enter the temperature in Fahrenhiet: ");
+        scanf("%f", &fahrenheit);
+        celsius = (fahrenheit - 32) * 5 / 9;
+        printf("%.1f Fahrenheit is equal to %.1f Celsius", fahrenheit, celsius);
+    }
+    else
+    {
+        printf("Invalid choice! Please select C or F\n");
+    }
+    return 0;
+}
