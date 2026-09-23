2249f0d5186bfbb124f150df04f196ef74e730b2
Alan Shaaban <alanbsh112@gmail.com>
2026-09-23 17:28:54 -0400

    Scratchpad

--- Scratchpad/main.c
+++ Scratchpad/main.c
@@ -5,6 +5,15 @@
 
 int main()
 {
+        float price = 10.00;
+        bool isStudent = true;
 
+        if (isStudent)
+        {
+                printf("You get a student discount of 10%\n");
+                price *= 0.9;
+        }
+
+        printf("The price of a ticket is : $%.2f\n", price);
         return 0;
 }
